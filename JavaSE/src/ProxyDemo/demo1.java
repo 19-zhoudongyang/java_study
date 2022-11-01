8fc41e3ae9609cb2f3773389526b31dcc23f9cb6
ZDy <975313032@qq.com>
2022-11-01 21:58:46 +0800

    2022/11/1

--- JavaSE/src/ProxyDemo/demo1.java
+++ JavaSE/src/ProxyDemo/demo1.java
@@ -4,8 +4,8 @@ import java.lang.reflect.Proxy;
 
 public class demo1 {
     public static void main(String[] args)  {
-        Proxydemo proxydemo = new Proxydemo(new User());
-        Person person = proxydemo.userProxy();
-        person.add();
+        int i=10;
+        i = i++;
+        System.out.println(i);
     }
 }
