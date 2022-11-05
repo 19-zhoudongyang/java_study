96d073fe793a71005359e338ff621e5c87e8c5a6
ZDy <975313032@qq.com>
2022-11-05 22:43:38 +0800

    2022/11/5

--- JavaSE/src/ProxyDemo/demo1.java
+++ JavaSE/src/ProxyDemo/demo1.java
@@ -1,11 +1,23 @@
 package ProxyDemo;
 
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class demo1 {
-    public static void main(String[] args)  {
-        int i=10;
-        i = i++;
-        System.out.println(i);
+//    byte[] array = new byte[1*1024];
+    public static void main(String[] args) {
+        ArrayList<demo1> list = new ArrayList<>();
+        int count = 0;
+        try{
+            while(true){
+                list.add(new demo1());
+                count = count+1;
+            }
+        }catch (Exception e){
+            System.out.println("count:"+count);
+            e.printStackTrace();
+        }
     }
 }
